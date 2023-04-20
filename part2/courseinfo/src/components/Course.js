96c7d59f28bb37fcc906c509937f2ed6db3486cd
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-04-20 00:19:58 -0500

    Course information step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -4,6 +4,7 @@ const Course = props => {
         <div>
             <h1>{props.course.name}</h1>
             {props.course.parts.map(e => <p key={e.id}>{e.name} {e.exercises}</p>)}
+            <p><b>total of {props.course.parts.reduce((acc,c) => acc+c.exercises,0)} exercises</b></p>
         </div>
 
         
