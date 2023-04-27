d55a5b00c6ae87ae02c227f72abe0130f3da94d0
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-04-27 13:26:17 -0500

    Exercise 2.8 The Phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-123456',
+  }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = event => {
     event.preventDefault()
@@ -15,14 +18,20 @@ const App = () => {
 
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = event => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -32,14 +41,14 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
       <ul>
-      {persons.map(person => <li>{person.name}</li>)}
+      {persons.map(person => <li>{person.name} {person.number}</li>)}
       </ul>
       
     </div>
